6a30e8086ab56e3f3114fad0570973dec336e097
jseo9732 <jseo9732@gmail.com>
2021-08-30 16:19:25 +0900

    2.2.1 변수를 상태값, 속성값으로 관리 하지않음

---
+++ 2-chapter/2.2/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+let color = 'red';
+export default function App() {
+  function onClick() {
+    color = 'blue';
+  }
+  return (
+    <button style={ { backgroundColor: color } } onClick={onClick}>
+      좋아요
+    </button>
+  );
+}
