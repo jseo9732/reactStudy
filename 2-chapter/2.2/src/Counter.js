a6566d4e622a66b184bb8ac98a1bd204636b0ad9
jseo9732 <jseo9732@gmail.com>
2021-08-30 18:40:11 +0900

    2.2.6 객체가 상태값일 때 불변변수로 관리

--- 2-chapter/2.2/src/Counter.js
+++ 2-chapter/2.2/src/Counter.js
@@ -2,19 +2,14 @@ import React, { useState } from 'react';
 import Title from './Title';
 
 export default function Counter() {
-    const [count, setCount] = useState(0);
-    const [count2, setCount2] = useState(0);
+    const [count, setCount] = useState({ value: 0 });
     function onClick() {
-        setCount(count + 1);
-    }
-    function onClick2() {
-        setCount2(count2 + 1);
+        setCount({ ...count, value: count.value + 1 });
     }
     return (
         <div>
-            <Title title={ `현재 카운트: ${count}` } /> 
+            <Title title={ `현재 카운트: ${count.value}` } /> 
             <button onClick={ onClick }>증가</button>
-            <button onClick={ onClick2 }>증가2</button>
         </div>
     );
 }
